0971aa53c96f140e45a3d230d23680904738a549
Oleksandr Baglai <oleksandr_baglai@epam.com>
2023-05-09 10:32:26 +0300

    [all][games] I make the name of the test method the same for all tests.

--- src/test/java/com/codenjoy/dojo/reversi/services/ScoresTest.java
+++ src/test/java/com/codenjoy/dojo/reversi/services/ScoresTest.java
@@ -84,7 +84,7 @@ public class ScoresTest {
     }
 
     @Test
-    public void shouldStillZeroAfterDead() {
+    public void shouldNotBeLessThanZero() {
         // given
         givenScores(0);
 
